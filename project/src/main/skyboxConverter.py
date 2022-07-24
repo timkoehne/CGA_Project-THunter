0e50654f00d4f92e9554ab67963cbb230e1b2424
Tim Köhne <tim.koehne97@gmail.com>
2022-07-24 10:59:03 +0200

    skybox converter kann eine zusammengefasste skybox textur in 6 einzelne texturen trennen

---
+++ project/src/main/skyboxConverter.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import sys
+
+image = Image.open(sys.argv[1])
+sides = ("", "top", "", "", "left", "front",
+         "right", "back", "", "bottom", "", "")
+
+new_width = image.width/4
+new_height = image.height/3
+
+for y in range(3):
+    for x in range(4):
+        if(sides[y*4+x] != ""):
+            print(sides[y*4+x])
+            tile = image.crop(
+                (new_width * x, new_height * y, new_width * x + new_width, new_height * y + new_height))
+            tile.save(sides[y*4+x]+".png")
